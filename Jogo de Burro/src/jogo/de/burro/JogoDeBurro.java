56cf93f8d16a3d8c57f31c87a1d48a52357ff82c
Helisonrt <helisonreus@gmail.com>
2012-09-26 19:14:03 -0300

    Classe principal criada

---
+++ Jogo de Burro/src/jogo/de/burro/JogoDeBurro.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogo.de.burro;
+
+/**
+ *
+ * @author Helison
+ */
+public class JogoDeBurro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
